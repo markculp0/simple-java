2a4d702e99e087d30b25d65ea1e97bd336d0952a
Mark Culp <markculp0@gmail.com>
2019-02-26 21:13:14 -0600

    HiLo game basic logic

--- HiLo/src/main/java/mc/hilo/App.java
+++ HiLo/src/main/java/mc/hilo/App.java
@@ -1,14 +1,35 @@
 package mc.hilo;
 
+import java.util.Scanner;
+
 /**
- * Hello world!
+ * HiLo Game
  *
  */
 public class App 
 {
     public static void main( String[] args )
-    {
+    {   
+        // Create input scanner
+        Scanner scan = new Scanner(System.in);
+      
+        // Create random number to guess
         int theNumber = (int)(Math.random() * 100 + 1);
-        System.out.println( theNumber );
-    }
-}
+        int guess = 0;  // Init guess
+        
+        while (guess != theNumber) {
+        
+          // Get the user's guess
+          System.out.print( "Guess number from 1 to 100: " );
+          guess = scan.nextInt();
+          
+          // Evaluate answer
+          if (guess < theNumber)
+            System.out.println(guess + " is too low.");
+          else if (guess > theNumber)
+            System.out.println(guess + " is too high.");
+          else
+            System.out.println(guess + " is correct.");
+        } // end while loop
+    } // end main method
+} // end App class
